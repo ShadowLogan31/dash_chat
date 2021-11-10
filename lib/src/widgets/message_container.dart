47cf11dc07d62d8e9d3c964c72d861a1f9db8646
ShadowLogan31 <48600104+ShadowLogan31@users.noreply.github.com>
2021-11-10 09:06:14 -0600

    Update message_container.dart

--- lib/src/widgets/message_container.dart
+++ lib/src/widgets/message_container.dart
@@ -197,7 +197,7 @@ class MessageContainer extends StatelessWidget {
                   width: constraints!.maxWidth * 0.7,
                   fit: BoxFit.contain,
                   placeholder: kTransparentImage,
-                  image: message.images!.elementAt(index).toString(),
+                  image: message.images?.elementAt(index).toString(),
                 ),
               );
             },
