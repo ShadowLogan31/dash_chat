9d4548316bc6d739f3184998fb8fc7bfeb56d9c1
ShadowLogan31 <48600104+ShadowLogan31@users.noreply.github.com>
2021-11-09 19:39:02 -0600

    Update message_container.dart

--- lib/src/widgets/message_container.dart
+++ lib/src/widgets/message_container.dart
@@ -179,9 +179,9 @@ class MessageContainer extends StatelessWidget {
 
   Widget _buildMessageImage() {
     if (message.images!.isNotEmpty) {
-      messageImageBuilder?.call(message.images, message) ??
+      return messageImageBuilder?.call(message.images, message) ??
           message.images!.forEach((element) {
-            return Padding(
+            Padding(
               padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
               child: FadeInImage.memoryNetwork(
                 height: constraints!.maxHeight * 0.3,
