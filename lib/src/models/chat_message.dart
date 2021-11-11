f5d3ca483baef647485ac733bbca6f62b16663c9
ShadowLogan31 <48600104+ShadowLogan31@users.noreply.github.com>
2021-11-11 02:42:55 -0600

    upgrade

--- lib/src/models/chat_message.dart
+++ lib/src/models/chat_message.dart
@@ -23,11 +23,11 @@ class ChatMessage {
 
   /// A [non-optional] parameter which is used to display images
   /// takes a [Sring] as a url
-  List<Object?>? images = [];
+  Map<String?, String?>? images = {};
 
   /// A [non-optional] parameter which is used to display vedio
   /// takes a [Sring] as a url
-  List<Object?>? videos = [];
+  Map<String?, String?> videos = {};
 
   /// A [non-optional] parameter which is used to show quick replies
   /// to the user
@@ -59,8 +59,8 @@ class ChatMessage {
   ChatMessage.fromJson(Map<dynamic, dynamic> json) {
     id = json['id'];
     text = json['text'];
-    images = json['images'] as List<Object?>?;
-    videos = json['videos'] ?? json['vedio'] as List<Object?>?;
+    images = json['images'] as Map<String?, String?>?;
+    videos = json['videos'] ?? json['vedio'] as Map<String?, String?>?;
     createdAt = DateTime.fromMillisecondsSinceEpoch(json['createdAt']);
     user = ChatUser.fromJson(json['user']);
     quickReplies = json['quickReplies'] != null
