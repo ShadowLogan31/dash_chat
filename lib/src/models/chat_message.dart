90e92afabf81d50238f73d5bd72d1247109c0f39
ShadowLogan31 <48600104+ShadowLogan31@users.noreply.github.com>
2021-11-11 01:49:40 -0600

    Update

--- lib/src/models/chat_message.dart
+++ lib/src/models/chat_message.dart
@@ -23,7 +23,7 @@ class ChatMessage {
 
   /// A [non-optional] parameter which is used to display images
   /// takes a [Sring] as a url
-  List<String?>? images = [];
+  List<Map<String, String>>? images = [];
 
   /// A [non-optional] parameter which is used to display vedio
   /// takes a [Sring] as a url
@@ -59,8 +59,8 @@ class ChatMessage {
   ChatMessage.fromJson(Map<dynamic, dynamic> json) {
     id = json['id'];
     text = json['text'];
-    images = json['images'] as List<Object?>?;
-    videos = json['videos'] ?? json['vedio'] as List<Object?>?;
+    images = json['images'] as List<Map<String, String>>;
+    videos = json['videos'] ?? json['vedio'];
     createdAt = DateTime.fromMillisecondsSinceEpoch(json['createdAt']);
     user = ChatUser.fromJson(json['user']);
     quickReplies = json['quickReplies'] != null
@@ -75,7 +75,7 @@ class ChatMessage {
     try {
       data['id'] = this.id;
       data['text'] = this.text;
-      data['images'] = jsonEncode(this.images);
+      data['images'] = this.images;
       data['videos'] = this.videos;
       data['createdAt'] = this.createdAt.millisecondsSinceEpoch;
       data['user'] = user.toJson();
