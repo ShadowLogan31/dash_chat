15281a2f233c5ce87e256d66832c1a2b799bbc46
ShadowLogan31 <48600104+ShadowLogan31@users.noreply.github.com>
2021-11-11 13:00:22 -0600

    Updated

--- lib/src/models/chat_message.dart
+++ lib/src/models/chat_message.dart
@@ -23,7 +23,7 @@ class ChatMessage {
 
   /// A [non-optional] parameter which is used to display images
   /// takes a [Sring] as a url
-  List<Object?>? images = [];
+  Map<String?, String?>? images;
 
   /// A [non-optional] parameter which is used to display vedio
   /// takes a [Sring] as a url
@@ -59,7 +59,7 @@ class ChatMessage {
   ChatMessage.fromJson(Map<dynamic, dynamic> json) {
     id = json['id'];
     text = json['text'];
-    images = json['images'] as List<Object?>?;
+    images = json['images'];
     videos = json['videos'] ?? json['vedio'] as List<Object?>?;
     createdAt = DateTime.fromMillisecondsSinceEpoch(json['createdAt']);
     user = ChatUser.fromJson(json['user']);
